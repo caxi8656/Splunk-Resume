89cdd2d1ef6e8e6b5cf2c9af6453b43dee2e4e57
caxi8656 <94191884+caxi8656@users.noreply.github.com>
2022-11-02 22:29:57 -0600

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovyan/project-2270-final-project-noname
-# Build directory: /home/jovyan/project-2270-final-project-noname/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
